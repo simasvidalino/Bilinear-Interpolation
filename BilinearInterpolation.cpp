76cede501eb285a7fa1e8f265e15b2dad17b1ee7
simasvidalino <78229980+simasvidalino@users.noreply.github.com>
2021-12-14 08:55:41 -0300

    Add files via upload
    BilinearInterpolation: delete comments

--- BilinearInterpolation.cpp
+++ BilinearInterpolation.cpp
@@ -5,14 +5,12 @@ BilinearInterpolation::BilinearInterpolation(std::vector<double> &x, std::vector
     y(y),
     z(z)
 {
-std::cout<<"cheguei "<<std::endl;
 }
 
 double BilinearInterpolation::getInterpolatedZ(double x, double y)
 {
     xValue = x;
     yValue = y;
-    std::cout<<"x "<<xValue<<" y "<< yValue<<std::endl;
     auto result = calculateBilinearInterpolate();
     return result;
 }
@@ -20,25 +18,18 @@ double BilinearInterpolation::getInterpolatedZ(double x, double y)
 void BilinearInterpolation::setBilinearInterpolateTerm(std::vector<double> &coodenates, double &value, double &a,
                                                        double &b, int &indexa, int &indexb)
 {
-    for (std::size_t i = 1; i < coodenates.size(); i++)
+    for (int i = 1; i < int(coodenates.size()); i++)
         if(coodenates[i-1] <= value && coodenates[i] >= value)
         {
            b = coodenates[i];
            a = coodenates[i-1];
            indexb = i;
            indexa = i-1;
-           std::cout<<"Valor "<<value<<" Maior "<<b<<" Menor "<<a<<"\na "<<a<<" b "<<b<<std::endl;
         }
 }
 
 double BilinearInterpolation::calculateBilinearInterpolate()
 {
-    for(auto value : x)
-        std::cout<<"x "<<value<<std::endl;
-
-    for(auto value : y)
-        std::cout<<"y "<<value<<std::endl;
-
     setBilinearInterpolateTerm(x, xValue, xa, xb, ixa, ixb);
     setBilinearInterpolateTerm(y, yValue, ya, yb, iya, iyb);
 
